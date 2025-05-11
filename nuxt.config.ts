2bf5832efcbc49588741ba7e37d214bddafd1e2c
Yaser M. Shabana <yaser.m.shabana@gmail.com>
2025-05-11 16:32:33 +0300

    feat: setup  tailwind theme and fonts - add the fonts and colors to main.css - remove tailwind package (it comes built in nuxt ui) - rename index.css to main.css

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,19 +1,12 @@
-import tailwindcss from "@tailwindcss/vite";
-
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  css: ['~/assets/css/index.css'],
-  vite: {
-    plugins: [
-      tailwindcss(),
-    ]
-  },
+  css: ['~/assets/css/main.css'],
   modules: [
     '@nuxt/fonts',
     '@nuxt/icon',
     '@nuxt/image',
     '@nuxt/test-utils',
-    '@nuxt/ui'
+    '@nuxt/ui',
   ]
 })
